2eb780ff106ac69d4620099f3b5a48bb95cd8082
Sam <sam@bytespritegames.com>
2023-09-28 18:48:25 +0800

    Work stuff with liam

--- Assets/ByteSprite/HeadsetDetectionVive.cs
+++ Assets/ByteSprite/HeadsetDetectionVive.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using Valve.VR;
+using UnityEngine.XR.OpenXR;
 
 public class HeadsetDetectionVive : MonoBehaviour {
+    [Tooltip("Action Reference that represents the control")]
+    [SerializeField] private InputActionReference _actionReference = null;
     bool wasWarn = false;
+    
     void Update() {
+        Debug.Log(_actionReference.action.ReadValue<float>());
+        
+        // Read the bit mask values of the htc vive headset:
+
+
         var headset = UnityEngine.XR.InputDevices.GetDeviceAtXRNode(UnityEngine.XR.XRNode.Head);
         // detect if headset is worn
         if (headset.TryGetFeatureValue(UnityEngine.XR.CommonUsages.userPresence, out bool headsetIsWorn)) {
